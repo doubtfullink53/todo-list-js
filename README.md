290d68f967e33e42446b3e9b9f54d8a24a79ba2b
Robby <doubtfullink53@gmail.com>
2023-01-02 12:45:09 -0500

    initial webpack set up with loaders for css, images, json

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # todo-list-js
+Theodinproject :https://www.theodinproject.com/lessons/node-path-javascript-todo-list
+
+
+The Todo List
+
+Todo lists are a staple in beginning webdev tutorials because they can be very simple. There is, however, a lot of room for improvement and many features that can be added.
+
+Uses webpack bundler 
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+import Data from './data.xml';
+import Notes from './data.csv';
+
+import json from './data.json5';
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+    
+    return element;
+  }
+  
+  document.body.appendChild(component());
